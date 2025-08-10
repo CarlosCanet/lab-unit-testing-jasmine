6402fa9eab6cad08c43800958e18643f06812168
Carlos Canet <carlos@carlosca.net>
2025-08-10 21:02:02 +0200

    fix bad copy paste

--- tests/divide.spec.js
+++ tests/divide.spec.js
@@ -25,14 +25,14 @@ describe("Iteration 2", () => {
         });
 
         it("should return undefined if any of the arguments is not a number", () => {
-            expect(add("a", 1)).toEqual(undefined);
-            expect(add(1, "ba")).toEqual(undefined);
-            expect(add(1, "2")).toEqual(undefined);
-            expect(add(true, 1)).toEqual(undefined);
-            expect(add(2, false)).toEqual(undefined);
-            expect(add(true, false)).toEqual(undefined);
-            expect(add(23, null)).toEqual(undefined);
-            expect(add(undefined, null)).toEqual(undefined);
+            expect(divide("a", 1)).toEqual(undefined);
+            expect(divide(1, "ba")).toEqual(undefined);
+            expect(divide(1, "2")).toEqual(undefined);
+            expect(divide(true, 1)).toEqual(undefined);
+            expect(divide(2, false)).toEqual(undefined);
+            expect(divide(true, false)).toEqual(undefined);
+            expect(divide(23, null)).toEqual(undefined);
+            expect(divide(undefined, null)).toEqual(undefined);
         });
     })    
 })
